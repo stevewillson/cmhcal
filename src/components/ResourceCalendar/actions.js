b4ba10c2b694f3084e91fb8a6b690f2f91968014
Steve Willson <steve.willson@gmail.com>
2021-09-29 10:50:42 -0700

    update to start events on days, not listing hours

--- src/components/ResourceCalendar/actions.js
+++ src/components/ResourceCalendar/actions.js
@@ -9,8 +9,8 @@ export const createEvent = (plainEventObject) => {
     type: 'CREATE_EVENT', 
     payload: {
       title: plainEventObject.title,
-      start: plainEventObject.start.toISOString().slice(0.10),
-      end: plainEventObject.end.toISOString().slice(0,10),
+      start: plainEventObject.startStr,
+      end: plainEventObject.endStr,
       id: plainEventObject.id,
       resourceId: plainEventObject.getResources()[0]._resource.id,
       url: plainEventObject.url,
